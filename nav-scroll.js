09e61c0677d80e530c30dc84ffe35e671ca11b12
thezigzag <pjbuonomo@me.com>
2023-04-25 15:02:21 -0400

    Cyber

---
+++ nav-scroll.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    // nav scroll	
+	if($('#dtr-header-global').length){
+		var navoffset = $('#dtr-header-global').height();
+		$('.dtr-nav a[href^="#"], .dtr-scroll-link').on("click", function(e) {
+			event.preventDefault();  
+			$('html, body').animate({
+				scrollTop: $($(this).attr('href')).offset().top - navoffset - 37
+			}, "slow","easeInSine");
+		});
+	} else {
+		$('.dtr-scroll-link').on("click", function(e) {
+			event.preventDefault();  
+			$('html, body').animate({
+				scrollTop: $($(this).attr('href')).offset().top
+			}, "slow","easeInSine");
+		});
+	}
+}); // document ready
