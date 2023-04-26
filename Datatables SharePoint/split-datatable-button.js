b336aae794ab1dd461c7955950863cdb385dc54a
thezigzag <pjbuonomo@me.com>
2023-04-26 17:09:35 -0400

    Update split-datatable-button.js

--- Datatables SharePoint/split-datatable-button.js
+++ Datatables SharePoint/split-datatable-button.js
@@ -1,13 +1,29 @@
+<html>
+  <head>
+  
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+ <div class="btn-group">
+  <button type="button" class="btn btn-primary">Regular Button</button>
+  <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+    <span class="visually-hidden">Toggle Dropdown</span>
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Link 1</a></li>
+    <li><a class="dropdown-item" href="#">Link 2</a></li>
+    <li><a class="dropdown-item" href="#">Link 3</a></li>
+  </ul>
+</div> 
+</body>
+
+</html>
+
 "mData": null,
-"render": function(data, type, row, meta) {
-  var dropdownHtml = '<div class="btn-group">' +
-                      '<button type="button" class="btn btn-secondary">Actions</button>' +
-                      '<button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
-                        '<span class="sr-only">Toggle Dropdown</span>' +
-                      '</button>' +
-                      '<div class="dropdown-menu">' +
-                        '<a class="dropdown-item" href="#">Action 1</a>' +
-                        '<a class="dropdown-item" href="#">Action 2</a>' +
-                      '</div>' +
-                    '</div>';
-  return dropdownHtml;
+  "render": function (data, type, row) {
+    var trashIcon = '<a href="www.example.com" style="text-decoration: none; margin-right: 10px;"><i class="fa fa-trash"></i></a>';
+    var editIcon = '<a href="www.example.com" style="text-decoration: none; margin-right: 10px;"><i class="fa fa-pencil"></i></a>';
+    var personIcon = '<a href="www.example.com" style="text-decoration: none;"><i class="fa fa-user"></i></a>';
+    return trashIcon + editIcon + personIcon;
