a0dd7074646a60d43af75524f01a48cfbbc3af67
thezigzag <pjbuonomo@me.com>
2023-04-26 16:23:27 -0400

    Update split-datatable-button.js

--- Datatables SharePoint/split-datatable-button.js
+++ Datatables SharePoint/split-datatable-button.js
@@ -1,3 +1,14 @@
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- Bootstrap JavaScript (popper.js is required for the dropdown) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/esm/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 {
     "mData": null,
     "render": function (data, type, row) {
